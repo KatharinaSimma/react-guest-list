6c3a49c16457ebab56d90d3ff43c6c581a08ce4f
Katharina Simma <katharina.simma@diebin.at>
2023-01-30 21:04:41 +0100

    Fix https for netlify

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 // export const baseUrl = 'http://localhost:4000';
-export const baseUrl = 'http://826ab240-e08a-46b6-b995-b48f29d8dc74.id.repl.co';
+export const baseUrl =
+  'https://826ab240-e08a-46b6-b995-b48f29d8dc74.id.repl.co/guests/';
