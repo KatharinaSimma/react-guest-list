0c95300968ee01831673fd1b41d1e901a26ab042
Katharina Simma <katharina.simma@diebin.at>
2023-01-30 21:07:18 +0100

    Fix baseUrl

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // export const baseUrl = 'http://localhost:4000';
 export const baseUrl =
-  'https://826ab240-e08a-46b6-b995-b48f29d8dc74.id.repl.co/guests/';
+  'https://826ab240-e08a-46b6-b995-b48f29d8dc74.id.repl.co';
