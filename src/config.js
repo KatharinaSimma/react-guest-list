aa322f3b3741293a34e81b926ef39163b7ec2ebe
Katharina Simma <katharina.simma@diebin.at>
2023-04-13 21:10:52 +0200

    Explain commented out code

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
+// use this for your local environment
 // export const baseUrl = 'http://localhost:4000';
+
+// use this for replit
 export const baseUrl =
   'https://826ab240-e08a-46b6-b995-b48f29d8dc74.id.repl.co';
